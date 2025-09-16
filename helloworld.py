ccb623fb24f3751611367ec1a9a33da146375418
Priyanshu Srivastava <118589098+priyanshu-0212@users.noreply.github.com>
2025-09-16 23:52:29 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,3 @@
 print("Hello World")
+#To print any output we are supposed to just simply do the print and put brackets and use the quotation and compile 
+#it and boom you printed your first output.

a942614fafd224d4bdcecf010cd396b1eea64b09
Priyanshu Srivastava <118589098+priyanshu-0212@users.noreply.github.com>
2025-09-18 00:07:06 +0530

    Create Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':           # runs this block only if file is executed directly
+    a = int(input())                # read first number from user (as integer)
+    b = int(input())                # read second number from user (as integer)
+
+    sum = a + b                     # add a and b, store in sum
+    dif = a - b                     # subtract b from a, store in dif
+    product = a * b                 # multiply a and b, store in product
+
+    print(sum)                      # print the sum
+    print(dif)                      # print the difference
+    print(product)                  # print the product

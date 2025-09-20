8ff788dffd6410d690a0c35279198bc759073c25
Priyanshu Srivastava <118589098+priyanshu-0212@users.noreply.github.com>
2025-09-21 00:34:28 +0530

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':          # This block ensures the code runs only when executed directly, not when imported.
+    n = int(input())                # Takes input from the user, converts it to an integer, and stores it in 'n'.
+
+for i in range(n):                  # Loops through all integers from 0 up to (n-1).
+    print(i**2)                     # Prints the square of the current number 'i'.
